13574af12313789a380f999e334acfc1eb14d75e
Vladislav Stefanov <vladcata22@abv.bg>
2020-06-18 03:43:39 +0300

    Fix StopwordFileLoader to be able to read stopwords from the jar
    Signed-off-by: Vladislav Stefanov <vladcata22@abv.bg>

--- BookmarkManagerServer/src/main/java/bg/sofia/uni/fmi/mjt/bookmarks/repositories/files/StopwordFileLoader.java
+++ BookmarkManagerServer/src/main/java/bg/sofia/uni/fmi/mjt/bookmarks/repositories/files/StopwordFileLoader.java
@@ -2,10 +2,8 @@ package bg.sofia.uni.fmi.mjt.bookmarks.repositories.files;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,29 +13,21 @@ import bg.sofia.uni.fmi.mjt.bookmarks.model.Tag;
 public class StopwordFileLoader {
 
     private static final String STOPWORDS_LOCATION = "stopwords.txt";
-
-    private final Path stopwordsPath;
+    private final InputStream inputStream;
 
     public StopwordFileLoader() {
-        Path defaultStopwordsPath;
-        try {
-            defaultStopwordsPath = Paths.get(StopwordFileLoader.class
-                    .getResource(STOPWORDS_LOCATION).toURI());
-        } catch (URISyntaxException e) {
-            defaultStopwordsPath = Paths.get(STOPWORDS_LOCATION);
-        }
-
-        stopwordsPath = defaultStopwordsPath;
+        this(StopwordFileLoader.class.getResourceAsStream(STOPWORDS_LOCATION));
     }
 
-    public StopwordFileLoader(final Path stopwordsLocation) {
-        stopwordsPath = stopwordsLocation;
+    public StopwordFileLoader(final InputStream inputStream) {
+        this.inputStream = inputStream;
     }
 
     public Set<Tag> loadStopwords() {
-        try (BufferedReader reader = Files.newBufferedReader(stopwordsPath)) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inputStream))) {
             return reader.lines().map(Tag::newTag).collect(Collectors.toSet());
-        } catch (IOException e) {
+        } catch (IOException | NullPointerException e1) {
             return new HashSet<>();
         }
     }
--- BookmarkManagerServer/src/test/java/bg/sofia/uni/fmi/mjt/bookmarks/repositories/files/StopwordFileLoaderTest.java
+++ BookmarkManagerServer/src/test/java/bg/sofia/uni/fmi/mjt/bookmarks/repositories/files/StopwordFileLoaderTest.java
@@ -3,14 +3,11 @@ package bg.sofia.uni.fmi.mjt.bookmarks.repositories.files;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import bg.sofia.uni.fmi.mjt.bookmarks.model.Tag;
@@ -20,25 +17,12 @@ public class StopwordFileLoaderTest {
     private static final Set<Tag> STOPWORDS = new HashSet<>(
             Arrays.asList(Tag.newTag("when"), Tag.newTag("then")));
 
-    private static Path stopwordsPath;
-    private static Path stopwordsWrongPath;
-
-    @BeforeClass
-    public static void setUpBeforeAllTests() {
-        try {
-            stopwordsPath = Paths.get(StopwordFileLoaderTest.class
-                    .getResource("testStopwords.txt").toURI());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        stopwordsWrongPath = Paths.get("testStopwordsWrong.txt");
-    }
-
     @Test
     public void testLoadStopwords() {
-        this.getClass().getResource("").getPath().toString();
+        InputStream inputStream = StopwordFileLoaderTest.class
+                .getResourceAsStream("testStopwords.txt");
         StopwordFileLoader stopwordFileLoader = new StopwordFileLoader(
-                stopwordsPath);
+                inputStream);
         Set<Tag> loadedStopwords = stopwordFileLoader.loadStopwords();
 
         assertEquals(STOPWORDS, loadedStopwords);
@@ -46,8 +30,7 @@ public class StopwordFileLoaderTest {
 
     @Test
     public void testLoadStopwordsWithWrongFile() {
-        StopwordFileLoader stopwordFileLoader = new StopwordFileLoader(
-                stopwordsWrongPath);
+        StopwordFileLoader stopwordFileLoader = new StopwordFileLoader(null);
         Set<Tag> loadedStopwords = stopwordFileLoader.loadStopwords();
 
         assertTrue(loadedStopwords.isEmpty());
